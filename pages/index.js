301347c3decf59b71626a76a5cba977fedb1a51f
EdwxnRh <78694324+EdwxnRh@users.noreply.github.com>
2021-12-29 17:37:27 +0100

    updated approach on the hero

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import Head from 'next/head';
 import Header from '../components/header';
+import { useState, useEffect } from 'react';
 
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [img, setImg] = useState('/assets/hero-title.svg');
+  useEffect(() => {
+    const interval = setInterval(() => {}, 2100);
+    return () => clearInterval(interval);
+  });
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,8 +20,11 @@ export default function Home() {
       <Header></Header>
       <main>
         <section className={styles.head}>
-          <div className={styles.bg}></div>
-          <h1 className={styles.title} id='one'>
+          <div
+            className={styles.bg}
+            style={{ backgroundImage: `url(${img})` }}
+          ></div>
+          {/* <h1 className={styles.title} id='one'>
             Design.
           </h1>
           <h1 className={styles.title} id='two'>
@@ -22,12 +32,51 @@ export default function Home() {
           </h1>
           <h1 className={styles.title} id='three'>
             Revise.
-          </h1>
-          <a className={styles.contact}>Let&#39;s work together - Contact me</a>
-          <p className={styles.intro}>
-            Hi, I am Edwin, a passionate designer and webdeveloper from Germany.
-            I create beautiful, responsive and modern solutions for your idea.
-          </p>
+          </h1> */}
+          <div className={styles.intro}>
+            <a className={styles.contact}>
+              Let&#39;s work together - Contact me
+            </a>
+            <p className={styles.introText}>
+              Hi, I am Edwin, a passionate designer and webdeveloper from
+              Germany. I create beautiful, responsive and modern solutions for
+              your idea.
+            </p>
+          </div>
+        </section>
+        <section className={styles.introSection}>
+          <div className={styles.introBox}>
+            <h2 className={styles.introTitle}>
+              Let&#39;s transform <br />
+              your idea into a <br />
+              solid product
+            </h2>
+            <p className={styles.introTextSt}>
+              I truly believe that good design is the key to success. <br />
+              "We eat with our eyes" not only applies to food but also to
+              everything around us. Our brain decides inseconds if we like
+              something or not, only based onthe first glaze.
+            </p>
+            <p className={styles.introText}>
+              Having not only a good-looking but also clear and inclusive design
+              is a necessity in today&#39;s world.
+              <br />
+              <br /> As a freelance UI & UX designer, I performed on numerous
+              projects as the chief designer, which permitted me to acquire an
+              adequate working speed. I specialized in inclusiveness and
+              creating reliable products using modern technologies. <br />
+              <br />
+              As web technologies gain more and more attention, great solutions
+              are coming out that developers have to catch up on. I have
+              resolved to supply my customers not only with a visually
+              satisfying but also a technically contemporary product. <br />
+              <br />
+              Creating things is a passion of mine and I want to use my
+              experience and creativity to help you realize your vision. <br />
+              <br />
+              Let&#39;s create the future together!
+            </p>
+          </div>
         </section>
       </main>
     </div>
